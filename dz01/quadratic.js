ec3879620817374e8dd2f78134d2f5cca055879d
grebennikovdevstark <a-greb@yandex.ru>
2024-03-02 10:36:47 +0300

    new line is not nesessary at least on windows

--- dz01/quadratic.js
+++ dz01/quadratic.js
@@ -3,12 +3,12 @@ import process, { argv, stderr, stdout } from 'node:process';
 const args = argv.slice(2);
 
 if (args.length > 3) {
-    stderr.write('More than three arguments detected\n');
+    stderr.write('More than three arguments detected');
     process.exit(1);
 }
 
 if (!args.length) {
-    stderr.write('You need to enter at least one first argument\n');
+    stderr.write('You need to enter at least one first argument');
     process.exit(1);
 }
 
@@ -17,12 +17,12 @@ const b = args[1] ? Number(args[1]) : 0;
 const c = args.length <= 2 ? 0 : Number(args[2]);
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
-    stderr.write('Please, enter proper numbers\n');
+    stderr.write('Please, enter proper numbers');
     process.exit(1);
 }
 
 if (a === 0) {
-    stderr.write('The first argument should not be zero\n');
+    stderr.write('The first argument should not be zero');
     process.exit(1);
 } else {
     stdout.write('Arguments ok\n');
@@ -32,12 +32,12 @@ if (a === 0) {
         const sqrtDiscriminant = Math.sqrt(discriminant);
         const results = [(-b + sqrtDiscriminant) / (2 * a), (-b - sqrtDiscriminant) / (2 * a)];
         results.sort();
-        stdout.write(`There are two roots: ${results[0].toFixed(2)} ${results[1].toFixed(2)}\n`);
+        stdout.write(`There are two roots: ${results[0].toFixed(2)} ${results[1].toFixed(2)}`);
     } else if (discriminant === 0) {
         const result = -b / (2 * a);
-        stdout.write(`There is one root: ${result.toFixed(2)}\n`);
+        stdout.write(`There is one root: ${result.toFixed(2)}`);
     } else {
-        stdout.write('There are no roots\n');
+        stdout.write('There are no roots');
     }
 
     process.exit(0);
